205dc281985bd493794da339d524ccf387a46287
KeyurMistry <keyurmist@gmail.com>
2022-04-25 22:08:12 +0100

    added arrays for the criteria

---
+++ 02-Homework/Develop/script.js
@@ -0,0 +1,33 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var number = ["0123456789"];
+var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lower = ["abcdefghijklmnopqrstuvwxyz"];
+var special = ["!#$%&'()*+,-./:;<=>?@\"[]^_`{|}~"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
